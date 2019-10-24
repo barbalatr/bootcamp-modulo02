c17ee3f07e77f67d267485e79243139e0f7a8c55
Rodrigo Barbalat <barbalatr@gmail.com>
2019-10-24 11:35:04 -0200

    finish vs code config

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-import express  from 'express';
-import routes  from './routes';
+import express from 'express';
+import routes from './routes';
 
 class App {
   constructor() {
     this.server = express();
-    
+
     this.middleware();
     this.routes();
   }
@@ -16,7 +16,6 @@ class App {
   routes() {
     this.server.use(routes);
   }
-
-};
+}
 
 export default new App().server;
